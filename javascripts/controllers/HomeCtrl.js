c34182a627ddc6a3a92a2b286b46185e439f397b
Jess Brawner <jessnbillbrawner@gmail.com>
2017-11-29 18:25:31 -0600

    removed console log

--- javascripts/controllers/HomeCtrl.js
+++ javascripts/controllers/HomeCtrl.js
@@ -5,7 +5,6 @@ app.controller("HomeCtrl", function ($location, $routeParams, $scope, PinService
 
     const getPins = () => {
         PinService.getAllPins().then((results) => {
-            console.log("all pins in home controller", results);
             $scope.pins = results;
         }).catch((error) => {
             console.log("error in getPins in HomeCtrl", error);
